2fef2137e0ad627fab441997c9df58ba1e1f113f
Elvan Hidayat <elvanhd@gmail.com>
2024-03-07 20:59:44 +0700

    Enhance Vuex usage with mutations for state updates
    - Update the button in App.vue to increment counter by 10
    - Add ChangeCounter component with functionality to add 2 to counter
    - Implement Vuex mutations for incrementing and increasing counter
    - Use commit method in App.vue and ChangeCounter.vue to dispatch mutations
    - Define 'increment' mutation for adding 2 to counter
    - Define 'increase' mutation with payload for flexible addition to counter
    - Utilize mutations in the Vuex store for state management

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
-    <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
+    ChangeCounter,
   },
   methods: {
     addOne() {
-      this.$store.state.counter++;
+      this.$store.commit({
+        type: 'increase',
+        value: 10,
+      });
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ const store = createStore({
       counter: 0,
     };
   },
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 2;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
 });
 
 const app = createApp(App);
