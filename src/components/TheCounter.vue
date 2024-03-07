1b86004d8e0146a21260f3c1efbabfdc616c5324
Elvan Hidayat <elvanhd@gmail.com>
2024-03-07 21:28:23 +0700

    Refactor Vuex usage with mapActions and mapGetters
    - Enhance ChangeCounter component to use mapActions for dispatching Vuex actions
      - Replace direct method calls with mapActions for 'increment' and 'increase'
      - Add a button to trigger the 'increase' action with a payload of 11
    - Update TheCounter component to utilize mapGetters for accessing Vuex state
      - Replace computed property with mapGetters to access the 'finalCounter' getter
    - Simplify code and improve readability by leveraging Vuex helpers

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,13 +1,13 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <h3>{{ finalCounter }}</h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.finalCounter;
-    },
+    ...mapGetters(['finalCounter']),
   },
 };
 </script>
